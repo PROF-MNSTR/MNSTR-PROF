dc3318975de29f972d9428a8828d54bd62b13c38
PROF-MNSTR <moumou2014c@gmail.com>
2025-07-26 11:39:37 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FFCoachDashboard from './FFCoachDashboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FFCoachDashboard />
+  </React.StrictMode>
+)
